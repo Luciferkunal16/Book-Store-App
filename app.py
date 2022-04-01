df158ad5bf79ad406f82ba0e60df89a333040538
Luciferkunal16 <Kunalbatham15@gmail.com>
2022-04-01 12:00:39 +0530

    Added UC3:Email service for sending otp is done

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.register_blueprint(example_blueprint)
 
 app.config.from_object("settings")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kunal123@localhost/bookstore'
 db = SQLAlchemy(app)
+
 
 if __name__ == '__main__':
     from user.views import *
